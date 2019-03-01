bc6dbc0f2e523bdf638ede5cfb17e3d481281258
Timdt <35102214+Timdt@users.noreply.github.com>
2019-03-01 10:03:01 +0100

    uploaded project files
    Uploaded files

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel panel-default">
+  <div class="panel-heading">
+    Create a new post
+  </div>
+  <div class="panel-body">
+    <form action="{{ route('post.store')}}" method="post">
+      {{ csrf_field() }}
+
+      <div class="form-group">
+
+        <label for="title">Title</label>
+
+        <input type="text" name="title" class="form-control">
+
+      </div>
+
+      <div class="form-group">
+
+        <label for="featured">Feutured Image</label>
+
+        <input type="file" name="featured" class="form-control">
+
+      </div>
+
+      <div class="form-group">
+
+        <label for="content">content</label>
+
+        <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+
+      </div>
+
+      <div class="form-group">
+        <div class="text-center">
+          <button class="btn btn-succes" type="submit">
+            store post
+          </button>
+        </div>
+      </div>
+
+
+
+    </form>
+  </div>
+</div>
+@stop
